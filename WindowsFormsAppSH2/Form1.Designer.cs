1642c2ec6aabe770f4806f119ce091b6ca6adb26
Alator2001 <starcev229@bk.ru>
2023-02-05 11:15:28 +0500

    def
    main

---
+++ WindowsFormsAppSH2/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace WindowsFormsAppSH2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button6 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Find COM";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(93, 14);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(156, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(355, 14);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Connect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(688, 767);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Write to file";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 43);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            series1.BackSecondaryColor = System.Drawing.Color.Transparent;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.Color.DarkMagenta;
+            series1.LabelBackColor = System.Drawing.Color.Transparent;
+            series1.LabelBorderColor = System.Drawing.Color.Transparent;
+            series1.LabelForeColor = System.Drawing.Color.White;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.MarkerBorderWidth = 3;
+            series2.Name = "Series2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(776, 375);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(688, 378);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(86, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Plot";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(292, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(57, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "9600";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(688, 796);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(86, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Live Plot";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(12, 424);
+            this.chart2.Name = "chart2";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart2.Series.Add(series3);
+            this.chart2.Size = new System.Drawing.Size(776, 405);
+            this.chart2.TabIndex = 8;
+            this.chart2.Text = "chart2";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(688, 349);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(86, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Open File...";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(688, 738);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(86, 23);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Save as...";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(255, 19);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(41, 13);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.Text = "Speed:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox3.Location = new System.Drawing.Point(750, 102);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(32, 20);
+            this.textBox3.TabIndex = 12;
+            this.textBox3.Text = "3";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Location = new System.Drawing.Point(655, 105);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(89, 13);
+            this.textBox4.TabIndex = 13;
+            this.textBox4.Text = "Size Window AVR:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 841);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ WindowsFormsAppSH2/Form1.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+
+namespace WindowsFormsAppSH2
+{
+
+    public partial class Form1 : Form
+    {
+
+        bool isConnected = false;
+        bool readingFlag = false;
+        bool plotFlag = false;
+
+        private int _countSeconds = 0;
+        string defpath = "C:\\Users\\starc\\source\\repos\\WindowsFormsAppSH2\\data.txt";
+        string path = "C:\\Users\\starc\\source\\repos\\WindowsFormsAppSH2\\data.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Обновление списка портов
+        {
+            comboBox1.Items.Clear();
+            // Получаем список COM портов доступных в системе
+            string[] portnames = SerialPort.GetPortNames();
+            // Проверяем есть ли доступные
+            if (portnames.Length == 0)
+            {
+                MessageBox.Show("COM PORT not found");
+            }
+            foreach (string portName in portnames)
+            {
+                //добавляем доступные COM порты в список           
+                comboBox1.Items.Add(portName);
+                Console.WriteLine(portnames.Length);
+                if (portnames[0] != null)
+                {
+                    comboBox1.SelectedItem = portnames[0];
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) // Статус подключения
+        {
+            if (!isConnected)
+            {
+                сonnectToBoard();
+            }
+            else
+            {
+                disconnectFromBoard();
+            }
+        }
+
+        private void сonnectToBoard()
+        {
+            string selectedPort = comboBox1.GetItemText(comboBox1.SelectedItem);
+            if (string.IsNullOrEmpty(selectedPort))
+            {
+                MessageBox.Show("Please select a COM port");
+            }
+            else
+            {
+                try
+                {
+                    isConnected = true;
+                    serialPort1.PortName = selectedPort;
+                    serialPort1.BaudRate = Convert.ToInt32(textBox1.Text);
+                    serialPort1.StopBits = StopBits.One;
+                    serialPort1.Open();
+                    button2.Text = "Disconnect";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error connecting to port: " + ex.Message);
+                    isConnected = false;
+                }
+            }
+        }
+
+        private void disconnectFromBoard()
+        {
+            isConnected = false;
+            serialPort1.Close();
+            button2.Text = "Connect";
+        }
+
+        private void button3_Click(object sender, EventArgs e) // Запись данных в файл
+        {
+            if (!isConnected)
+            {
+                MessageBox.Show("Device not connected");
+            }
+            else
+            {
+                Thread toFile = new Thread(readingClick);
+                if (!readingFlag)
+                {
+                    toFile.Start();
+                    button3.Text = "Stop writing to file...";
+                }
+                else
+                {
+                    readingFlag = false;
+                    MessageBox.Show("File writing stopped");
+                    button3.Text = "Write to file";
+                }
+            }
+        }
+
+        private void readingClick()
+        {
+            readingFlag = true;
+            string msg = "|";
+            File.AppendAllText(defpath, Environment.NewLine +  msg + Environment.NewLine);
+            while (readingFlag)
+            {
+                msg = serialPort1.ReadLine();
+                File.AppendAllText(defpath, msg);
+            }
+        }
+
+
+        private void button4_Click(object sender, EventArgs e) // построение графика
+        {
+            
+            this.chart1.Series[0].Points.Clear();
+            string[] msg = File.ReadAllLines(path);
+            double[] msgs = new double[msg.Length];
+            int j = 0;
+            for (int i = 0; i < msg.Length; i++)
+            {
+                if (msg[i] == "" || msg[i] == "|")
+                {
+                    continue;
+                }
+                else
+                {
+                    try
+                    {
+                        msgs[j] = Convert.ToDouble(msg[i]);
+                        this.chart1.Series[0].Points.AddXY(j, msg[i]);
+                        j++;
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                }
+            }
+            Avr();
+        }
+
+        private void button5_Click(object sender, EventArgs e) // Live Plot
+        {
+            chart2.ChartAreas[0].AxisX.LabelStyle.Format = "H:mm:ss";
+            chart2.Series[0].XValueType = ChartValueType.DateTime;
+            chart2.ChartAreas[0].AxisX.Minimum = DateTime.Now.ToOADate();
+            chart2.ChartAreas[0].AxisX.Maximum = DateTime.Now.AddMinutes(1).ToOADate();
+            chart2.ChartAreas[0].AxisX.IntervalType = DateTimeIntervalType.Seconds;
+            chart2.ChartAreas[0].AxisX.Interval = 5;
+            if (!plotFlag)
+            {
+                plotFlag = true;
+                timer1.Enabled = true;
+                button5.Text = "Stop";
+            }
+            else
+            {
+                plotFlag = false;
+                timer1.Enabled = false;
+                button5.Text = "Live Plot";
+            }
+            
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                string check = File.ReadAllLines(defpath).Last();
+                Convert.ToDouble(check);
+            }
+            catch
+            {
+                return;
+            }
+            string msg = File.ReadAllLines(defpath).Last();
+            DateTime timeNow = DateTime.Now;
+            chart2.Series[0].Points.AddXY(timeNow, msg);
+            _countSeconds++;
+            if (_countSeconds == 60)
+            {
+                _countSeconds = 0;
+                chart2.ChartAreas[0].AxisX.Minimum = DateTime.Now.ToOADate();
+                chart2.ChartAreas[0].AxisX.Maximum = DateTime.Now.AddMinutes(1).ToOADate();
+                chart2.ChartAreas[0].AxisX.IntervalType = DateTimeIntervalType.Seconds;
+                chart2.ChartAreas[0].AxisX.Interval = 5;
+            }
+           
+        }
+
+        private void button6_Click(object sender, EventArgs e) //Open File
+        {
+            if (openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                path = openFileDialog1.FileName;
+            }
+        }
+
+
+        private void button7_Click(object sender, EventArgs e) // Save as
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                path = saveFileDialog1.FileName;
+                string[] msg = File.ReadAllLines(defpath);
+                int stopSession = msg.Length - 1;
+                for (int i = msg.Length - 1; i >= 0; i--)
+                {
+                    if (msg[i]=="|")
+                    {
+                        stopSession = i;
+                        break;
+                    }
+                }
+                for (int i = stopSession + 1; i < msg.Length; i++)
+                {
+                    File.AppendAllText(path, msg[i] + Environment.NewLine);
+                }
+            }
+        }
+
+        //private void Avr() //Average
+        //{
+        //    int sizeWindow = Convert.ToInt32(textBox3.Text);
+        //    this.chart1.Series[1].Points.Clear();
+        //    double[] arrWindow = new double[sizeWindow];
+        //    string[] msgs = File.ReadAllLines(path);
+        //    double[] msg = msgs.Where(x => double.TryParse(x, out _)).Select(x => double.Parse(x)).ToArray();
+        //    for (int i = 0; i < msg.Length; i++)
+        //    {
+        //        for (int j = 0; j < sizeWindow - 1; j++)
+        //        {
+        //            arrWindow[j] = arrWindow[j + 1];
+        //        }
+        //        arrWindow[sizeWindow - 1] = msg[i];
+        //        double avr = Math.Sqrt(arrWindow.Sum(x => x * x) / sizeWindow);
+        //        this.chart1.Series[1].Points.AddXY(i, avr);
+        //    }
+        //}
+
+        //private void Avr()
+        //{
+        //    int sizeWindow = Convert.ToInt32(textBox3.Text);
+        //    this.chart1.Series[1].Points.Clear();
+        //    double[] arrWindow = new double[sizeWindow];
+        //    string[] msgs = File.ReadAllLines(path);
+        //    double sum = 0;
+        //    int k = 0;
+        //    for (int i = 0; i < msgs.Length; i++)
+        //    {
+        //        if (msgs[i] == "|" || msgs[i] == "")
+        //        {
+        //            continue;
+        //        }
+        //        double msg;
+        //        if (double.TryParse(msgs[i], out msg))
+        //        {
+        //            sum += msg * msg;
+        //            k++;
+        //            if (k >= sizeWindow)
+        //            {
+        //                double avr = Math.Sqrt(sum / sizeWindow);
+        //                this.chart1.Series[1].Points.AddXY(i - sizeWindow + 1, avr);
+        //                sum -= arrWindow[k % sizeWindow] * arrWindow[k % sizeWindow];
+        //            }
+        //            arrWindow[k % sizeWindow] = msg;
+        //        }
+        //    }
+        //}
+
+
+
+
+
+        private void Avr()
+        {
+            try 
+            { 
+                Convert.ToUInt32(textBox3.Text); 
+            }
+            catch 
+            { 
+                MessageBox.Show("Please enter a valid value Size Window AVR");
+                return;
+            }
+            uint sizeWindow = Convert.ToUInt32(textBox3.Text);
+            this.chart1.Series[1].Points.Clear();
+            double[] arrWindow = new double[sizeWindow];
+            string[] msgs = File.ReadAllLines(path);
+            double[] msg = new double[msgs.Length];
+            int k = 0;
+            for (int i = 0; i < msgs.Length; i++)
+            {
+                try
+                {
+                    if (msgs[i] == "|" || msgs[i] == "")
+                    {
+                        continue;
+                    }
+                    msg[k] = Convert.ToDouble(msgs[i]);
+                    k++;
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+            for (int i = 0; i < k; i++)
+            {
+                for (int j = 0; j < sizeWindow; j++)
+                {
+                    if (j == sizeWindow - 1)
+                    {
+                        arrWindow[j] = msg[i];
+                    }
+                    else
+                    {
+                        arrWindow[j] = arrWindow[j + 1];
+                    }
+                }
+                double sum = 0;
+                for (int j = 0; j < sizeWindow; j++)
+                {
+                    sum += arrWindow[j] * arrWindow[j];
+                }
+                double avr = Math.Sqrt(sum / sizeWindow);
+                this.chart1.Series[1].Points.AddXY(i, avr);
+            }
+
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
